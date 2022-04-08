d78565d30122ca9c7b1eab8c292e9f445601dd7f
Luisandre12 <99758302+Luisandre12@users.noreply.github.com>
2022-04-08 16:49:05 -0300

    Segunda aula

---
+++ modelo.php
@@ -0,0 +1,33 @@
+<?php
+
+    //inclui o cabeçalo da página e se de erro vai para o programa.
+    require('header.php');
+
+    //inclui o cabeçalo da página e se de erro vai continuar o programa.
+    //include('_header.php');
+
+?>    
+<section>
+<h2>Título da pagina</h2>   
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem dignissimos officiis sapiente, aut alias!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem dignissimos officiis sapiente, aut alias!</p>
+
+</section>
+
+<aside>
+
+<h3>Lateral</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliqu</p>
+
+</aside>
+
+<?php
+
+    //inclui o rodapé da página e se de erro vai para o programa.
+    require('_footer.php');
+
+    
+    //inclui o rodapé da página e se de erro vai continuar o programa.
+    //include('_header.php')
+
+?>
